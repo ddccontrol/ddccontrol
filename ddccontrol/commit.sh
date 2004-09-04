62585a526b5dcecffde293cb334a5c488c7576aa
Nicolas Boichat <nboichat@users.sourceforge.net>
2004-09-04 21:14:09 +0000

    Use GMT time instead of local time in ChangeLog

--- ddccontrol/commit.sh
+++ ddccontrol/commit.sh
@@ -2,7 +2,7 @@
 echo "Committing your changes (message: '$1')..."
 cvs commit -m "$1"
 echo "Updating ChangeLog..."
-cvs2cl.pl -I ChangeLog
+cvs2cl.pl --gmt -I ChangeLog
 echo "Committing ChangeLog..."
 cvs commit -m "Update ChangeLog" ChangeLog
 echo "OK"
