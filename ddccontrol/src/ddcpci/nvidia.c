063d5365ba33b05a5662ec4d16d4de7a7710b0c2
Nicolas Boichat <nboichat@users.sourceforge.net>
2004-11-07 09:03:59 +0000

    Add original copyright to nvidia.c.

--- ddccontrol/src/ddcpci/nvidia.c
+++ ddccontrol/src/ddcpci/nvidia.c
@@ -2,6 +2,10 @@
     ddc/ci direct PCI memory interface for nvidia cards
     Copyright(c) 2004 Nicolas Boichat (nicolas@boichat.ch)
 
+    Based on rivafb-i2c.c from the kernel source.
+    
+    Copyright 2004 Antonino A. Daplas <adaplas @pol.net>
+    
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
