783c529ba7537ec7ebfdbd0fb2b54261aee45e87
Nicolas Boichat <nboichat@users.sourceforge.net>
2005-04-01 18:30:38 +0000

    Add tag names in ChangeLog.

--- ddccontrol/commit.sh
+++ ddccontrol/commit.sh
@@ -2,7 +2,7 @@
 echo "Committing your changes (message: '$1')..."
 cvs commit -m "$1"
 echo "Updating ChangeLog..."
-cvs2cl.pl --gmt -I ChangeLog
+cvs2cl.pl -T --gmt -I ChangeLog
 echo "Committing ChangeLog..."
 cvs commit -m "Update ChangeLog" ChangeLog
 echo "OK"
