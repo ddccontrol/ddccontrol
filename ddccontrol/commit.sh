57ccf6fb912520340ce47aab2fcef699671f3ab8
el_cubano <el_cubano@users.sourceforge.net>
2007-10-28 02:31:40 +0000

    Fix commit script to work with subversion

--- ddccontrol/commit.sh
+++ ddccontrol/commit.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+echo "Updating working copy..."
+svn update || exit 1
 echo "Committing your changes (message: '$1')..."
 svn commit -m "$1"
 echo "Updating ChangeLog..."
-(TZ=GMT svn2cl -i -r COMMITTED --authors=AUTHORS --stdout; cat ChangeLog) >ChangeLog.tmp && mv -f ChangeLog.tmp ChangeLog
+svn update
+PREVIOUS_REV=$(expr `svn info |grep ^Revision |cut -f2 -d' '` - 1)
+(TZ=GMT svn2cl -i -r $PREVIOUS_REV --authors=AUTHORS --stdout; cat ChangeLog) >ChangeLog.tmp && mv -f ChangeLog.tmp ChangeLog
 echo "Committing ChangeLog..."
 svn commit -m "Update ChangeLog" ChangeLog
 echo "OK"
