818d04aceaa15649c716479e6e15268a3335ee1d
el_cubano <el_cubano@users.sourceforge.net>
2007-10-28 02:34:37 +0000

    Tweak script to get correct log entry

--- ddccontrol/commit.sh
+++ ddccontrol/commit.sh
@@ -5,8 +5,8 @@ echo "Committing your changes (message: '$1')..."
 svn commit -m "$1"
 echo "Updating ChangeLog..."
 svn update
-PREVIOUS_REV=$(expr `svn info |grep ^Revision |cut -f2 -d' '` - 1)
-(TZ=GMT svn2cl -i -r $PREVIOUS_REV --authors=AUTHORS --stdout; cat ChangeLog) >ChangeLog.tmp && mv -f ChangeLog.tmp ChangeLog
+REVISION=$(svn info |grep ^Revision |cut -f2 -d' ')
+(TZ=GMT svn2cl -i -r $REVISION --authors=AUTHORS --stdout; cat ChangeLog) >ChangeLog.tmp && mv -f ChangeLog.tmp ChangeLog
 echo "Committing ChangeLog..."
 svn commit -m "Update ChangeLog" ChangeLog
 echo "OK"
