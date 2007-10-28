6e170b1f01068f7b2312f53dae6968107df92762
el_cubano <el_cubano@users.sourceforge.net>
2007-10-28 04:21:49 +0000

    Allow options from the environment

--- ddccontrol-web/commit.sh
+++ ddccontrol-web/commit.sh
@@ -4,11 +4,11 @@ make
 echo "Cleaning..."
 make clean
 echo "Committing your changes to root files (message: '$1')..."
-svn commit -m "$1"
+svn commit $SVN_OPTS -m "$1"
 echo "Remaking..."
 make
 echo "Committing updated html files and other files (message: '$1')..."
-cvs commit -m "$1"
+cvs commit $SVN_OPTS -m "$1"
 echo "Updating website..."
 make update
 echo "OK"
