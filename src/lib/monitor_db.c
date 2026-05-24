657d8206e8a5b45186c53b8ee1f4b2d6adb087e0
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2026-05-24 14:12:19 +0200

    Suppress noise for missing monitor profiles

--- src/lib/monitor_db.c
+++ src/lib/monitor_db.c
@@ -454,12 +454,10 @@ int ddcci_create_db_protected(
 	 */
 	if (access(buffer, R_OK) != 0) {
 		if (errno == ENOENT) {
-			fprintf(stderr, _("No database entry found for monitor ID %s.\n"), pnpname);
-			fprintf(stderr, _("This monitor is not yet supported by ddccontrol-db.\n"));
-			fprintf(stderr, _("Basic DDC communication may still work.\n"));
+			return 0;
 		}
 		else {
-			fprintf(stderr, _("Unable to access monitor database file %s: %s.\n"), buffer, strerror(errno));
+			fprintf(stderr, _("Cannot access monitor/%s.xml: %s\n"), pnpname, strerror(errno));
 		}
 		return 0;
 	}
